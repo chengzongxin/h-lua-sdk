81e1d7463e3e2e2b3040b3682bbddbe4c4da21fb
hunzsig <mzyhaohaoren@qq.com>
2021-11-27 10:48:32 +0800

    feat(hslk): n2v re补充

--- depend/h-lua/lib/slk.lua
+++ depend/h-lua/lib/slk.lua
@@ -1,11 +1,29 @@
 ---@class hslk slk专用管理
 hslk = {}
 
+--- 根据名称获取ID
+--- 根据名称只对应一个ID，返回string
+--- 根据名称如对应多个ID，返回table
+---@param name string
+---@return string|string[]|nil
+hslk.n2i = function(name)
+    if (type(name) ~= "string") then
+        return
+    end
+    if (HSLK_N2I[name] == nil or type(HSLK_N2I[name]) ~= "table") then
+        return
+    end
+    if (#HSLK_N2I[name] == 1) then
+        return HSLK_N2I[name][1]
+    end
+    return HSLK_N2I[name]
+end
+
 --- 根据ID获取slk数据（包含原生的slk和hslk）
 --- 原生的slk数值键值是根据地图编辑器作为标准的，所以大小写也是与之一致
 ---@param id string|number
----@vararg string 可选，直接获取级层key的值，如 hslk.i2v("slk","Primary") == "STR"
----@return table|nil
+---@vararg string 可选，直接获取级层key的值，如 hslk.i2v("H001","slk","Primary") == "STR"
+---@return table|any
 hslk.i2v = function(id, ...)
     if (id == nil) then
         return
@@ -37,38 +55,21 @@ end
 
 --- 根据名称获取slk数据（包含原生的slk和hslk）
 --- 原生的slk数值键值是根据地图编辑器作为标准的，所以大小写也是与之一致
---- 如果名称冲突，则只能获取到最后设定的数据
+--- 如果名称冲突重复，则获取到数据集
 ---@param name string
----@return table|nil
-hslk.n2v = function(name)
-    if (type(name) ~= "string") then
-        return
+---@vararg string 可选，直接获取级层key的值，如 hslk.n2v("天选勇者","slk","Primary") == "STR"
+---@return table|table[]|any
+hslk.n2v = function(name, ...)
+    local id = hslk.n2i(name)
+    if (type(id) == "string") then
+        return hslk.i2v(id, ...)
+    elseif (type(id) == "table") then
+        local v = {}
+        for _, i in ipairs(id) do
+            table.insert(v, hslk.i2v(i, ...))
+        end
+        return v
     end
-    if (HSLK_N2V[name] == nil or type(HSLK_N2V[name]) ~= "table") then
-        return
-    end
-    if (#HSLK_N2V[name] == 1) then
-        return HSLK_N2V[name][1]
-    end
-    return HSLK_N2V[name]
-end
-
---- 根据名称获取ID
---- 根据名称只对应一个ID，返回string
---- 根据名称如对应多个ID，返回table
----@param name string
----@return string|table|nil
-hslk.n2i = function(name)
-    if (type(name) ~= "string") then
-        return
-    end
-    if (HSLK_N2I[name] == nil or type(HSLK_N2I[name]) ~= "table") then
-        return
-    end
-    if (#HSLK_N2I[name] == 1) then
-        return HSLK_N2I[name][1]
-    end
-    return HSLK_N2I[name]
 end
 
 --- 根据 hslk._class 获取ID集
